87fdd308628601970389a5c998e2c077f34c623d
MichaelPhelps217 <157077931+MichaelPhelps217@users.noreply.github.com>
2024-03-07 08:35:45 +0000

    question #2 (midterm)

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,37 @@
+#include "question2.h"
+
+int get_int_from_user()
+{
+	int user_input = 0;
+	std::cout<<"\nPlease enter a number between 0-255: ";
+	std::cin>>user_input;
+
+	return user_input;
+}
+
+bool validate_user_input(int user_input)
+{
+	if(user_input > 0 && user_input <= 255)
+	{
+    	return user_input;
+	}
+    return false;
+}
 
 int main()
 {
+    char option = 'q';
+    do
+	{
+        int inputed_choice = get_int_from_user();
+        if(!validate_user_input(inputed_choice))
+            {
+                std::cout<<"Invalid Option"<<"\n";
+                return 0;
+            }
 
-    return 0;
+        std::cout<<decimal_to_binary(inputed_choice)<<"\n";
+        std::cout<<"Continue (y/n)? ";
+		std::cin>>option;
+	}while(option == 'y');
 }
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -4,3 +4,15 @@ bool test_config()
 {
     return true;
 }
+
+string decimal_to_binary(int value)
+{
+    string rval = "";
+    for(int i = 7; i >= 0; i--)
+    {
+        //shifts value by i (decrements each loop) then compare zero bit (right most bit in value) to 1 (&1). If that is true then append '1', else append '0'.
+        rval.push_back((value>>i)&1 ? '1' : '0');
+
+    }
+    return rval;
+}
